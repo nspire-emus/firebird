6f0b9f30bf0910b9a781a1701c381292337c0305
Fabian Vogt <fabian@ritter-vogt.de>
2015-11-07 20:54:07 +0100

    Fix warning about redefining PAGE_SIZE define in mmu.h

--- core/mmu.c
+++ core/mmu.c
@@ -195,29 +195,29 @@ static void addr_cache_invalidate(int i) {
  * at a time, we can keep only a few pages committed and thereby reduce
  * the memory used by a lot. */
 #define AC_COMMIT_MAX 128
-#define PAGE_SIZE 4096
-uint8_t ac_commit_map[AC_NUM_ENTRIES * sizeof(ac_entry) / PAGE_SIZE];
+#define AC_PAGE_SIZE 4096
+uint8_t ac_commit_map[AC_NUM_ENTRIES * sizeof(ac_entry) / AC_PAGE_SIZE];
 ac_entry *ac_commit_list[AC_COMMIT_MAX];
 uint32_t ac_commit_index;
 
 bool addr_cache_pagefault(void *addr) {
-    ac_entry *page = (ac_entry *)((uintptr_t)addr & -PAGE_SIZE);
+    ac_entry *page = (ac_entry *)((uintptr_t)addr & -AC_PAGE_SIZE);
     uint32_t offset = page - addr_cache;
     if (offset >= AC_NUM_ENTRIES)
         return false;
     ac_entry *oldpage = ac_commit_list[ac_commit_index];
     if (oldpage) {
         //printf("Freeing %p, ", oldpage);
-        os_sparse_decommit(oldpage, PAGE_SIZE);
-        ac_commit_map[offset / (PAGE_SIZE / sizeof(ac_entry))] = 0;
+        os_sparse_decommit(oldpage, AC_PAGE_SIZE);
+        ac_commit_map[offset / (AC_PAGE_SIZE / sizeof(ac_entry))] = 0;
     }
     //printf("Committing %p\n", page);
-    if (!os_sparse_commit(page, PAGE_SIZE))
+    if (!os_sparse_commit(page, AC_PAGE_SIZE))
         return false;
-    ac_commit_map[offset / (PAGE_SIZE / sizeof(ac_entry))] = 1;
+    ac_commit_map[offset / (AC_PAGE_SIZE / sizeof(ac_entry))] = 1;
 
     uint32_t i;
-    for (i = 0; i < (PAGE_SIZE / sizeof(ac_entry)); i++)
+    for (i = 0; i < (AC_PAGE_SIZE / sizeof(ac_entry)); i++)
         addr_cache_invalidate(offset + i);
 
     ac_commit_list[ac_commit_index] = page;
@@ -238,7 +238,7 @@ void *addr_cache_miss(uint32_t virt, bool writing, fault_proc *fault) {
     }
     uint32_t oldoffset = ac_valid_list[ac_valid_index];
     uint32_t offset = (virt >> 10) * 2 + writing;
-    //if (ac_commit_map[oldoffset / (PAGE_SIZE / sizeof(ac_entry))])
+    //if (ac_commit_map[oldoffset / (AC_PAGE_SIZE / sizeof(ac_entry))])
     addr_cache_invalidate(oldoffset);
     addr_cache[offset] = entry;
     ac_valid_list[ac_valid_index] = offset;
@@ -258,7 +258,7 @@ void addr_cache_flush() {
 
     for (i = 0; i < AC_VALID_MAX; i++) {
         uint32_t offset = ac_valid_list[i];
-        //	if (ac_commit_map[offset / (PAGE_SIZE / sizeof(ac_entry))])
+        //	if (ac_commit_map[offset / (AC_PAGE_SIZE / sizeof(ac_entry))])
         addr_cache_invalidate(offset);
     }
 
