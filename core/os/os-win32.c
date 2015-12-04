4cb3591a9cbfec0d3b9006fa0dc582cce798a38e
Adrien Bertrand <bertrand.adrien@gmail.com>
2015-12-04 17:39:31 -0500

    Implement fopen_utf8 on Windows

--- core/os/os-win32.c
+++ core/os/os-win32.c
@@ -11,6 +11,18 @@
 #include "../emu.h"
 #include "../mmu.h"
 
+static HANDLE flash_mapping;
+static int flash_fd;
+
+FILE *fopen_utf8(const char *filename, const char *mode)
+{
+    wchar_t filename_w[MAX_PATH];
+    wchar_t mode_w[5];
+    MultiByteToWideChar(CP_UTF8, 0, filename, -1, filename_w, MAX_PATH);
+    MultiByteToWideChar(CP_UTF8, 0, mode, -1, mode_w, 5);
+    return _wfopen(filename_w, mode_w);
+}
+
 void *os_reserve(size_t size)
 {
     return VirtualAlloc(NULL, size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
