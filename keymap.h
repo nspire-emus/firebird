42c1158326ee9522edff348deb34fbb8f1214c86
denisps <avashurov@gmail.com>
2016-05-02 15:20:00 -0700

    Update keymap.h

--- keymap.h
+++ keymap.h
@@ -5,113 +5,117 @@
 
 #include <QKeyEvent>
 
-struct HostKey {
-    Qt::Key key;
-    QString name;
-    bool alt;
+int keyToKeypad(QKeyEvent *event);
+
+enum {
+    ret,    enter,  none,   neg,    space,  az,     ay,     n0,     punct,  on,     none1,
+    ax,     aw,     av,     n3,     au,     at,     as,     n1,     pi,     trig,   pow10_,
+    ar,     aq,     ap,     n6,     ao,     an,     am,     n4,     ee,     squ,    none2,
+    al,     ak,     aj,     n9,     ai,     ah,     ag,     n7,     div_,   exp_,   none3,
+    af,     ae,     ad,     none4,  ac,     ab,     aa,     equ,    mult,   pow_,   none5,
+    none6,  var,    minus,  pright, dot,    pleft,  n5,     cat,    metrix, del,    pad,
+    flag,   none7,  plus,   doc,    n2,     menu,   n8,     esc,    none8,  tab,    none9,
+    none10, none11, none12, none13, none14, none15, none16, none17, shift,  ctrl,   comma
 };
 
-#define KEY(x, y) static HostKey x{Qt::Key_##y, QStringLiteral(#y), false}
-#define ALT(x, y, z) static HostKey x{Qt::Key_##y,QStringLiteral(z), true}
-
-KEY(enter, Enter);
-KEY(tab, Tab);
-KEY(ret, Return);
-KEY(space, Space);
-KEY(shift, Shift);
-KEY(ctrl, Control);
-KEY(backspace, Backspace);
-KEY(escape, Escape);
-KEY(dot, Period);
-
-KEY(aa, A);
-KEY(ab, B);
-KEY(ac, C);
-KEY(ad, D);
-KEY(ae, E);
-KEY(af, F);
-KEY(ag, G);
-KEY(ah, H);
-KEY(ai, I);
-KEY(aj, J);
-KEY(ak, K);
-KEY(al, L);
-KEY(am, M);
-KEY(an, N);
-KEY(ao, O);
-KEY(ap, P);
-KEY(aq, Q);
-KEY(ar, R);
-KEY(as, S);
-KEY(at, T);
-KEY(au, U);
-KEY(av, V);
-KEY(aw, W);
-KEY(ax, X);
-KEY(ay, Y);
-KEY(az, Z);
-
-KEY(n0, 0);
-KEY(n1, 1);
-KEY(n2, 2);
-KEY(n3, 3);
-KEY(n4, 4);
-KEY(n5, 5);
-KEY(n6, 6);
-KEY(n7, 7);
-KEY(n8, 8);
-KEY(n9, 9);
-
-KEY(minus, Minus);
-KEY(plus, Plus);
-KEY(paren_left, ParenLeft);
-KEY(paren_right, ParenRight);
-
-KEY(comma, Comma);
-KEY(period, Period);
-
-KEY(f1, F1);
-KEY(f2, F2);
-KEY(f3, F3);
-KEY(f4, F4);
-KEY(f5, F5);
-KEY(f6, F6);
-KEY(f7, F7);
-KEY(f8, F8);
-KEY(f9, F9);
-
-ALT(negative, Minus, "(-)");
-ALT(wtf, W, "?!");
-ALT(pi, P, "pi");
-ALT(trig, T, "trig");
-ALT(pow10_, 1, "10^x");
-ALT(ee, E, "EE");
-ALT(squ, 2, "x^2");
-ALT(exp_, X, "e^x");
-ALT(var, V, "var");
-ALT(flag, F, "flag");
-ALT(frac, O, "frac");
-ALT(doc, D, "doc");
-ALT(menu, M, "menu");
-ALT(cat, C, "cat");
-ALT(scratch, S, "scratch");
-ALT(equ, Q, "=");
-ALT(ast, A, "*");
-ALT(pow_, P, "^");
-ALT(on, Escape, "On");
-
-HostKey none{static_cast<Qt::Key>(0), QStringLiteral(""), false};
-
-HostKey keymap_tp[8][11] =
+int keyToKeypad(QKeyEvent *event)
 {
-{ ret, enter, none, negative, space, az, ay, n0, wtf, on, none },
-{ ax, aw, av, n3, au, at, as, n1, pi, trig, pow10_ },
-{ ar, aq, ap, n6, ao, an, am, n4, ee, squ, none },
-{ al, ak, aj, n9, ai, ah, ag, n7, f3, exp_, none },
-{ af, ae, ad, none, ac, ab, aa, equ, ast, pow_, none },
-{ none, var, minus, f2, dot, f1, n5, cat, frac, backspace, scratch },
-{ flag, none, plus, doc, n2, menu, n8, escape, none, tab, none },
-{ none, none, none, none, none, none, none, none, shift, ctrl, comma }
-};
+    static const int ALT   = 0x02000000;
+
+    // Prefer virtual key, as it can be keyboard depandent key such as ParenLeft
+    auto button = event->key();
+
+    // Compose Alt into the unused bit of the keycode
+    if (event->modifiers() & Qt::AltModifier)
+        button |= ALT;
+
+    switch (button)
+    {
+        // Touchpad left buttons
+    case Qt::Key_Escape: case Qt::Key_Escape | ALT: return esc;
+    case Qt::Key_End: case Qt::Key_S | ALT: return pad;
+    case Qt::Key_Tab: return tab;
+
+        // Touchpad right buttons
+    case Qt::Key_Home: return on;
+    case Qt::Key_PageUp: case Qt::Key_D | ALT: return doc;
+    case Qt::Key_PageDown: case Qt::Key_M | ALT: return menu;
+
+        // Touchpad bottom buttons
+    case Qt::Key_Control: return ctrl;
+    case Qt::Key_Shift: return shift;
+    case Qt::Key_Insert: case Qt::Key_V | ALT: return var;
+    case Qt::Key_Backspace: case Qt::Key_Delete: return del;
+
+        // Alpha buttons
+    case Qt::Key_A: return aa;
+    case Qt::Key_B: return ab;
+    case Qt::Key_C: return ac;
+    case Qt::Key_D: return ad;
+    case Qt::Key_E: return ae;
+    case Qt::Key_F: return af;
+    case Qt::Key_G: return ag;
+    case Qt::Key_H: return ah;
+    case Qt::Key_I: return ai;
+    case Qt::Key_J: return aj;
+    case Qt::Key_K: return ak;
+    case Qt::Key_L: return al;
+    case Qt::Key_M: return am;
+    case Qt::Key_N: return an;
+    case Qt::Key_O: return ao;
+    case Qt::Key_P: return ap;
+    case Qt::Key_Q: return aq;
+    case Qt::Key_R: return ar;
+    case Qt::Key_S: return as;
+    case Qt::Key_T: return at;
+    case Qt::Key_U: return au;
+    case Qt::Key_V: return av;
+    case Qt::Key_W: return aw;
+    case Qt::Key_X: return ax;
+    case Qt::Key_Y: return ay;
+    case Qt::Key_Z: return az;
+    case Qt::Key_Less: case Qt::Key_E | ALT: return ee;
+    case Qt::Key_Bar: return pi;
+    case Qt::Key_Comma: return comma;
+    case Qt::Key_Question: case Qt::Key_W | ALT: return punct;
+    case Qt::Key_Greater: case Qt::Key_F | ALT: return flag;
+    case Qt::Key_Space: return space;
+    case Qt::Key_Enter | ALT: case Qt::Key_Return | ALT: return ret;
+
+        // Numpad buttons
+    case Qt::Key_0: return n0;
+    case Qt::Key_1: return n1;
+    case Qt::Key_2: return n2;
+    case Qt::Key_3: return n3;
+    case Qt::Key_4: return n4;
+    case Qt::Key_5: return n5;
+    case Qt::Key_6: return n6;
+    case Qt::Key_7: return n7;
+    case Qt::Key_8: return n8;
+    case Qt::Key_9: return n9;
+    case Qt::Key_Period: return dot;
+    case Qt::Key_Minus | ALT: case Qt::Key_QuoteLeft: return neg;
+
+        // Left buttons
+    case Qt::Key_Equal: case Qt::Key_Q | ALT: return equ;
+    case Qt::Key_Backslash: case Qt::Key_T | ALT: return trig;
+    case Qt::Key_AsciiCircum: case Qt::Key_P | ALT: return pow_;
+    case Qt::Key_At: case Qt::Key_2 | ALT: return squ;
+    case Qt::Key_BracketLeft: case Qt::Key_X | ALT: return exp_;
+    case Qt::Key_BracketRight: case Qt::Key_1 | ALT: return pow10_;
+    case Qt::Key_ParenLeft: case Qt::Key_F1: return pleft;
+    case Qt::Key_ParenRight: case Qt::Key_F2: return pright;
+
+        // Right buttons
+    case Qt::Key_Semicolon: case Qt::Key_O | ALT: return metrix;
+    case Qt::Key_Apostrophe: case Qt::Key_C | ALT: return cat;
+    case Qt::Key_Asterisk: case Qt::Key_A | ALT: return mult;
+    case Qt::Key_Slash: case Qt::Key_F3: return div_;
+    case Qt::Key_Plus: case Qt::Key_Equal | ALT: return plus;
+    case Qt::Key_Minus: case Qt::Key_Underscore: return minus;
+    case Qt::Key_Enter: case Qt::Key_Return: return enter;
+    }
+    return -1;
+}
 
 #endif // KEYMAP_H
