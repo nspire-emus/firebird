5d2bb522f2a3a2d6950aca007c70ced9fcb09b36
denisps <avashurov@gmail.com>
2016-05-06 14:39:16 -0700

    Update qtkeypadbridge.cpp

--- qtkeypadbridge.cpp
+++ qtkeypadbridge.cpp
@@ -3,137 +3,182 @@
 #include "keymap.h"
 #include "core/keypad.h"
 #include "qmlbridge.h"
+#include <QHash>
 
 QtKeypadBridge qt_keypad_bridge;
 
-int keyToKeypad(QKeyEvent *event)
-{
-    static const int ALT   = 0x02000000;
-
-    // Prefer virtual key, as it can be keyboard depandent key such as ParenLeft
-    auto button = event->key();
-
-    // Compose Alt into the unused bit of the keycode
-    if (event->modifiers() & Qt::AltModifier)
-        button |= ALT;
-
-    switch (button)
-    {
-        // Touchpad left buttons
-    case Qt::Key_Escape: return keymap::esc;
-    case Qt::Key_End: case Qt::Key_S | ALT: return keymap::pad;
-    case Qt::Key_Tab: return keymap::tab;
-
-        // Touchpad right buttons
-    case Qt::Key_Home: case Qt::Key_Escape | ALT: return keymap::on;
-    case Qt::Key_PageUp: case Qt::Key_D | ALT: return keymap::doc;
-    case Qt::Key_PageDown: case Qt::Key_M | ALT: return keymap::menu;
-
-        // Touchpad bottom buttons
-    case Qt::Key_Control: return keymap::ctrl;
-    case Qt::Key_Shift: return keymap::shift;
-    case Qt::Key_Insert: case Qt::Key_V | ALT: return keymap::var;
-    case Qt::Key_Backspace: case Qt::Key_Delete: return keymap::del;
-
-        // Alpha buttons
-    case Qt::Key_A: return keymap::aa;
-    case Qt::Key_B: return keymap::ab;
-    case Qt::Key_C: return keymap::ac;
-    case Qt::Key_D: return keymap::ad;
-    case Qt::Key_E: return keymap::ae;
-    case Qt::Key_F: return keymap::af;
-    case Qt::Key_G: return keymap::ag;
-    case Qt::Key_H: return keymap::ah;
-    case Qt::Key_I: return keymap::ai;
-    case Qt::Key_J: return keymap::aj;
-    case Qt::Key_K: return keymap::ak;
-    case Qt::Key_L: return keymap::al;
-    case Qt::Key_M: return keymap::am;
-    case Qt::Key_N: return keymap::an;
-    case Qt::Key_O: return keymap::ao;
-    case Qt::Key_P: return keymap::ap;
-    case Qt::Key_Q: return keymap::aq;
-    case Qt::Key_R: return keymap::ar;
-    case Qt::Key_S: return keymap::as;
-    case Qt::Key_T: return keymap::at;
-    case Qt::Key_U: return keymap::au;
-    case Qt::Key_V: return keymap::av;
-    case Qt::Key_W: return keymap::aw;
-    case Qt::Key_X: return keymap::ax;
-    case Qt::Key_Y: return keymap::ay;
-    case Qt::Key_Z: return keymap::az;
-    case Qt::Key_Less: case Qt::Key_E | ALT: return keymap::ee;
-    case Qt::Key_Bar: return keymap::pi;
-    case Qt::Key_Comma: return keymap::comma;
-    case Qt::Key_Question: case Qt::Key_W | ALT: return keymap::punct;
-    case Qt::Key_Greater: case Qt::Key_F | ALT: return keymap::flag;
-    case Qt::Key_Space: return keymap::space;
-    case Qt::Key_Enter | ALT: case Qt::Key_Return | ALT: return keymap::ret;
-
-        // Numpad buttons
-    case Qt::Key_0: return keymap::n0;
-    case Qt::Key_1: return keymap::n1;
-    case Qt::Key_2: return keymap::n2;
-    case Qt::Key_3: return keymap::n3;
-    case Qt::Key_4: return keymap::n4;
-    case Qt::Key_5: return keymap::n5;
-    case Qt::Key_6: return keymap::n6;
-    case Qt::Key_7: return keymap::n7;
-    case Qt::Key_8: return keymap::n8;
-    case Qt::Key_9: return keymap::n9;
-    case Qt::Key_Period: return keymap::dot;
-    case Qt::Key_Minus | ALT: case Qt::Key_QuoteLeft: return keymap::neg;
-
-        // Left buttons
-    case Qt::Key_Equal: case Qt::Key_Q | ALT: return keymap::equ;
-    case Qt::Key_Backslash: case Qt::Key_T | ALT: return keymap::trig;
-    case Qt::Key_AsciiCircum: case Qt::Key_P | ALT: return keymap::pow;
-    case Qt::Key_At: case Qt::Key_2 | ALT: return keymap::squ;
-    case Qt::Key_BracketLeft: case Qt::Key_X | ALT: return keymap::exp;
-    case Qt::Key_BracketRight: case Qt::Key_1 | ALT: return keymap::pow10;
-    case Qt::Key_ParenLeft: case Qt::Key_F1: return keymap::pleft;
-    case Qt::Key_ParenRight: case Qt::Key_F2: return keymap::pright;
-
-        // Right buttons
-    case Qt::Key_Semicolon: case Qt::Key_O | ALT: return keymap::metrix;
-    case Qt::Key_Apostrophe: case Qt::Key_C | ALT: return keymap::cat;
-    case Qt::Key_Asterisk: case Qt::Key_A | ALT: return keymap::mult;
-    case Qt::Key_Slash: case Qt::Key_F3: return keymap::div;
-    case Qt::Key_Plus: case Qt::Key_Equal | ALT: return keymap::plus;
-    case Qt::Key_Minus: case Qt::Key_Underscore: return keymap::minus;
-    case Qt::Key_Enter: case Qt::Key_Return: return keymap::enter;
-    }
-    return keymap::nobtn;
-}
-
 void setKeypad(int key, bool state)
 {
-    static int last_row;
-    static int last_col;
-
     int row = key / 11;
     int col = key % 11;
 
     if (state) {
-        keypad.key_map[row] |= 1 << col;
-        last_row = row;
-        last_col = col;
-    } else {
-        // If Shift was the only key pressed, then next operation is nop.
-        // If another key was pressed and released, then it also is nop.
-        // But if a shift modifiable key was and still is pressed when
-        // shift is released, then we need to release that key too because we
-        // are not going to get a release event for that key without shift.
-        if (key == keymap::shift || key == keymap::ctrl) {
-            keypad.key_map[last_row] &= ~(1 << last_col);
-            notifyKeypadStateChanged(last_row, last_col, state);
-        }
+        if (key == keymap::on)
+            keypad_on_pressed();
 
-        keypad.key_map[row] &= ~(1 << col);
+        keypad.key_map[row] |= 1 << col;
     }
+    else
+        keypad.key_map[row] &= ~(1 << col);
 
     notifyKeypadStateChanged(row, col, state);
     keypad_int_check();
+}
+
+void keyToKeypad(QKeyEvent *event)
+{
+    static const int ALT   = 0x02000000;
+    static const QHash<int, int> QtKeyMap {
+            // Touchpad left buttons
+        {Qt::Key_Escape, keymap::esc}
+        ,{Qt::Key_End, keymap::pad}
+        ,{Qt::Key_Tab, keymap::tab}
+
+            // Touchpad right buttons
+        ,{Qt::Key_Home, keymap::on}
+        ,{Qt::Key_Escape | ALT, keymap::on}
+        ,{Qt::Key_PageUp, keymap::doc}
+        ,{Qt::Key_D | ALT, keymap::doc}
+        ,{Qt::Key_PageDown, keymap::menu}
+        ,{Qt::Key_M | ALT, keymap::menu}
+
+            // Touchpad bottom buttons
+        ,{Qt::Key_Control, keymap::ctrl}
+        ,{Qt::Key_Shift, keymap::shift}
+        ,{Qt::Key_Insert, keymap::var}
+        ,{Qt::Key_V | ALT, keymap::var}
+        ,{Qt::Key_Backspace, keymap::del}
+        ,{Qt::Key_Delete, keymap::del}
+
+            // Alpha buttons
+        ,{Qt::Key_A, keymap::aa}
+        ,{Qt::Key_B, keymap::ab}
+        ,{Qt::Key_C, keymap::ac}
+        ,{Qt::Key_D, keymap::ad}
+        ,{Qt::Key_E, keymap::ae}
+        ,{Qt::Key_F, keymap::af}
+        ,{Qt::Key_G, keymap::ag}
+        ,{Qt::Key_H, keymap::ah}
+        ,{Qt::Key_I, keymap::ai}
+        ,{Qt::Key_J, keymap::aj}
+        ,{Qt::Key_K, keymap::ak}
+        ,{Qt::Key_L, keymap::al}
+        ,{Qt::Key_M, keymap::am}
+        ,{Qt::Key_N, keymap::an}
+        ,{Qt::Key_O, keymap::ao}
+        ,{Qt::Key_P, keymap::ap}
+        ,{Qt::Key_Q, keymap::aq}
+        ,{Qt::Key_R, keymap::ar}
+        ,{Qt::Key_S, keymap::as}
+        ,{Qt::Key_T, keymap::at}
+        ,{Qt::Key_U, keymap::au}
+        ,{Qt::Key_V, keymap::av}
+        ,{Qt::Key_W, keymap::aw}
+        ,{Qt::Key_X, keymap::ax}
+        ,{Qt::Key_Y, keymap::ay}
+        ,{Qt::Key_Z, keymap::az}
+        ,{Qt::Key_Less, keymap::ee}
+        ,{Qt::Key_E | ALT, keymap::ee}
+        ,{Qt::Key_Bar, keymap::pi}
+        ,{Qt::Key_Comma, keymap::comma}
+        ,{Qt::Key_Question, keymap::punct}
+        ,{Qt::Key_W | ALT, keymap::punct}
+        ,{Qt::Key_Greater, keymap::flag}
+        ,{Qt::Key_F | ALT, keymap::flag}
+        ,{Qt::Key_Space, keymap::space}
+        ,{Qt::Key_Enter | ALT, keymap::ret}
+        ,{Qt::Key_Return | ALT, keymap::ret}
+
+            // Numpad buttons
+        ,{Qt::Key_0, keymap::n0}
+        ,{Qt::Key_1, keymap::n1}
+        ,{Qt::Key_2, keymap::n2}
+        ,{Qt::Key_3, keymap::n3}
+        ,{Qt::Key_4, keymap::n4}
+        ,{Qt::Key_5, keymap::n5}
+        ,{Qt::Key_6, keymap::n6}
+        ,{Qt::Key_7, keymap::n7}
+        ,{Qt::Key_8, keymap::n8}
+        ,{Qt::Key_9, keymap::n9}
+        ,{Qt::Key_Period, keymap::dot}
+        ,{Qt::Key_Minus | ALT, keymap::neg}
+        ,{Qt::Key_QuoteLeft, keymap::neg}
+
+            // Left buttons
+        ,{Qt::Key_Equal, keymap::equ}
+        ,{Qt::Key_Q | ALT, keymap::equ}
+        ,{Qt::Key_Backslash, keymap::trig}
+        ,{Qt::Key_T | ALT, keymap::trig}
+        ,{Qt::Key_AsciiCircum, keymap::pow}
+        ,{Qt::Key_P | ALT, keymap::pow}
+        ,{Qt::Key_At, keymap::squ}
+        ,{Qt::Key_2 | ALT, keymap::squ}
+        ,{Qt::Key_BracketLeft, keymap::exp}
+        ,{Qt::Key_X | ALT, keymap::exp}
+        ,{Qt::Key_BracketRight, keymap::pow10}
+        ,{Qt::Key_1 | ALT, keymap::pow10}
+        ,{Qt::Key_ParenLeft, keymap::pleft}
+        ,{Qt::Key_F1, keymap::pleft}
+        ,{Qt::Key_ParenRight, keymap::pright}
+        ,{Qt::Key_F2, keymap::pright}
+
+            // Right buttons
+        ,{Qt::Key_Semicolon, keymap::metrix}
+        ,{Qt::Key_O | ALT, keymap::metrix}
+        ,{Qt::Key_Apostrophe, keymap::cat}
+        ,{Qt::Key_C | ALT, keymap::cat}
+        ,{Qt::Key_Asterisk, keymap::mult}
+        ,{Qt::Key_A | ALT, keymap::mult}
+        ,{Qt::Key_Slash, keymap::div}
+        ,{Qt::Key_F3, keymap::div}
+        ,{Qt::Key_Plus, keymap::plus}
+        ,{Qt::Key_Equal | ALT, keymap::plus}
+        ,{Qt::Key_Minus, keymap::minus}
+        ,{Qt::Key_Underscore, keymap::minus}
+        ,{Qt::Key_Enter, keymap::enter}
+        ,{Qt::Key_Return, keymap::enter}
+    };
+    static QHash<int, int> pressed_keys;
+
+    // Ignore autorepeat, calc os must handle it on it's own
+    if (event->isAutoRepeat())
+        return;
+
+    // Determine virtual key that correspond to the key we got
+    auto vkey = event->nativeVirtualKey();
+
+    // nativeVirtualKey should be working everywhere, but just in case it's not
+    if (vkey == 0 || vkey == 1)
+        vkey = event->nativeScanCode();
+
+    // If neither of them worked then simply use key code
+    if (vkey == 0 || vkey == 1)
+        vkey = event->key();
+
+    auto pressed = pressed_keys.find(vkey);
+
+    // If vkey is already pressed, then this must the the release event
+    if (pressed != pressed_keys.end())
+    {
+        setKeypad(*pressed, false);
+        pressed_keys.erase(pressed);
+    }
+    else if (event->type() == QEvent::KeyPress) // But press only on the press event
+    {
+        auto mkey = event->key();
+
+        // Compose alt into the unused bit of the keycode
+        if (event->modifiers() & Qt::AltModifier)
+            mkey |= ALT;
+
+        auto translated = QtKeyMap.find(mkey);
+
+        if (translated != QtKeyMap.end())
+        {
+            pressed_keys.insert(vkey, *translated);
+            setKeypad(*translated, true);
+        }
+    }
 }
 
 void QtKeypadBridge::keyPressEvent(QKeyEvent *event)
@@ -159,14 +204,7 @@ void QtKeypadBridge::keyPressEvent(QKeyEvent *event)
         keypad.touchpad_x = TOUCHPAD_X_MAX;
         break;
     default:
-        int button = keyToKeypad(event);
-        if (button != keymap::nobtn)
-        {
-            if (button == keymap::on)
-                keypad_on_pressed();
-
-            setKeypad(button, true);
-        }
+        keyToKeypad(event);
 
         return;
     }
@@ -205,9 +243,7 @@ void QtKeypadBridge::keyReleaseEvent(QKeyEvent *event)
             keypad.touchpad_contact = keypad.touchpad_down = false;
         break;
     default:
-        int button = keyToKeypad(event);
-        if (button != keymap::nobtn)
-            setKeypad(button, false);
+        keyToKeypad(event);
 
         return;
     }
