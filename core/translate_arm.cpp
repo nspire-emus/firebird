80cadd8e6f20780a6691bb900168b9a44bd0c722
Fabian Vogt <fabian@ritter-vogt.de>
2016-10-15 16:39:58 +0200

    [ARM] JIT: Remove caching
    - Made booting over 20% slower somehow

--- core/translate_arm.cpp
+++ core/translate_arm.cpp
@@ -158,50 +158,10 @@ static __attribute__((unused)) void emit_bkpt()
     emit_al(0x1200070); // bkpt #0
 }
 
-// Cache for translated code
-struct translation_cache_entry {
-    translation_cache_entry() : translation_cache_entry(nullptr, 0) {}
-    translation_cache_entry(uint32_t *ptr, size_t size) : ptr(ptr), size(size) {}
-    uint32_t *ptr;
-    size_t size;
-};
-
-uint32_t translation_cache_bits[0x10000000/32];
-uint32_t translation_cache_data[0x400000];
-uint32_t *translation_cache_data_current = translation_cache_data;
-std::unordered_map<uint32_t, translation_cache_entry> translation_cache;
-
-translation_cache_entry *translation_cache_query(Instruction i)
-{
-    uint32_t index = i.raw & 0xFFFFFFF; // Mask away condition code
-    if(!(translation_cache_bits[index / 32] & (1 << (index & 0x1F))))
-        return nullptr; // Not in cache
-
-    return &translation_cache.at(index);
-}
-
-void translation_cache_add(Instruction i, uint32_t *start, uint32_t *end)
-{
-    if(translation_cache_data_current - translation_cache_data + end - start >= 0x400000)
-        return; // Cache full!
-
-    uint32_t *data = translation_cache_data_current;
-    size_t size = end - start;
-    while(start < end)
-        *translation_cache_data_current++ = *start++;
-
-    uint32_t index = i.raw & 0xFFFFFFF;
-    translation_cache.emplace(std::piecewise_construct, std::forward_as_tuple(index), std::forward_as_tuple(data, size));
-
-    translation_cache_bits[index / 32] |= 1 << (index & 0x1F);
-}
-
 bool translate_init()
 {
     if(translate_buffer)
         return true;
-
-    std::fill(translation_cache_bits, translation_cache_bits + 0x10000000/32, 0);
 
     translate_current = translate_buffer = reinterpret_cast<uint32_t*>(os_alloc_executable(INSN_BUFFER_SIZE));
     translate_end = translate_current + INSN_BUFFER_SIZE/sizeof(*translate_buffer);
@@ -238,10 +198,6 @@ void translate(uint32_t pc_start, uint32_t *insn_ptr_start)
     translation *this_translation = &translation_table[next_translation_index];
     // Whether to stop translating code
     bool stop_here = false;
-    // Start of instruction translation, without cond. jump
-    uint32_t *translate_buffer_inst_real = translate_current;
-    // Whether this instruction can be cached (not possible if it is pc-dependant)
-    bool cachable = true;
 
     // We know this already. end_ptr will be set after the loop
     this_translation->jump_table = reinterpret_cast<void**>(jump_table_start);
@@ -276,19 +232,6 @@ void translate(uint32_t pc_start, uint32_t *insn_ptr_start)
             cond_branch = translate_current;
             emit(0x0a000000 | ((i.cond ^ 1) << 28));
         }
-
-        struct translation_cache_entry *cache = translation_cache_query(i);
-        if(cache)
-        {
-            auto cache_local = *cache;;
-            while(cache_local.size--)
-                emit(*cache_local.ptr++);
-
-            goto instruction_cachehit;
-        }
-
-        translate_buffer_inst_real = translate_current;
-        cachable = true;
 
         if((insn & 0xE000090) == 0x0000090)
         {
@@ -435,7 +378,6 @@ void translate(uint32_t pc_start, uint32_t *insn_ptr_start)
                 //B(L)X
                 if(i.bx.l)
                 {
-                    cachable = false; // Not cachable, as using PC
                     emit_mov(R0, pc + 4);
                     emit_str_armreg(R0, LR);
                 }
@@ -557,13 +499,9 @@ void translate(uint32_t pc_start, uint32_t *insn_ptr_start)
             if(i.mem_proc.rn != PC)
                 emit_ldr_armreg(R0, i.mem_proc.rn);
             else if(i.mem_proc.not_imm)
-            {
-                cachable = false;
                 emit_mov(R0, pc + 8);
-            }
             else // Address known
             {
-                cachable = false;
                 int offset = i.mem_proc.u ? i.mem_proc.immed :
                                             -i.mem_proc.immed;
                 unsigned int address = pc + 8 + offset;
@@ -618,10 +556,7 @@ void translate(uint32_t pc_start, uint32_t *insn_ptr_start)
                 if(i.mem_proc.rm != PC)
                     emit_ldr_armreg(R1, i.mem_proc.rm);
                 else
-                {
-                    cachable = false;
                     emit_mov(R1, pc + 8);
-                }
 
                 emit(off.raw);
             }
@@ -662,10 +597,7 @@ void translate(uint32_t pc_start, uint32_t *insn_ptr_start)
                 if(i.mem_proc.rd != PC)
                     emit_ldr_armreg(R1, i.mem_proc.rd); // r1 is the value
                 else
-                {
-                    cachable = false;
                     emit_mov(R1, pc + 12);
-                }
 
                 emit_call(reinterpret_cast<void*>(i.mem_proc.b ? write_byte_asm : write_word_asm));
             }
@@ -746,10 +678,7 @@ void translate(uint32_t pc_start, uint32_t *insn_ptr_start)
                 else
                 {
                     if(reg == PC)
-                    {
-                        cachable = false;
                         emit_mov(R1, pc + 12);
-                    }
                     else
                         emit_ldr_armreg(R1, reg);
 
@@ -784,8 +713,6 @@ void translate(uint32_t pc_start, uint32_t *insn_ptr_start)
             /* Branches work this way:
              * Either jump to translation_next if code not translated (yet) or
              * jump directly to the translated code, over a small function checking for pending events */
-
-            cachable = false; // Always pc-relative
 
             if(i.branch.l)
             {
@@ -825,11 +752,6 @@ void translate(uint32_t pc_start, uint32_t *insn_ptr_start)
             goto unimpl;
 
         instruction_translated:
-        // Instruction not in cache, cache it
-        if(cachable)
-            translation_cache_add(i, translate_buffer_inst_real, translate_current);
-
-        instruction_cachehit:
 
         if(cond_branch)
         {
