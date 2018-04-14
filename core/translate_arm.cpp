12960ca96203b441fb85c65d255d25523d9e3412
Fabian Vogt <fabian@ritter-vogt.de>
2018-04-14 09:50:45 +0200

    [ARM JIT] Remove the ldr/str duplication hack again
    - Doesn't actually give a speed advantage
    - Was missing a check for pre/post indexed addressing anyway

--- core/translate_arm.cpp
+++ core/translate_arm.cpp
@@ -751,12 +751,6 @@ void translate(uint32_t pc_start, uint32_t *insn_ptr_start)
             if(i.mem_proc.rn == PC && !i.mem_proc.p)
                 goto unimpl; // Writeback into PC not implemented
 
-            if(insn_ptr != insn_ptr_start && (insn_ptr[-1] ^ 0x100000) == i.raw)
-            {
-                can_jump_here = false;
-                goto instruction_translated;
-            }
-
             bool offset_is_zero = !i.mem_proc.not_imm && i.mem_proc.immed == 0;
 
             // Base register gets in r0
