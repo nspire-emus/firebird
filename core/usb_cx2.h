4125f170582bff9941748ff49b7e6a13c844de61
Jacob Young <jacobly0@users.noreply.github.com>
2019-10-03 23:39:26 +0200

    More usb fixes, now can receive and probably send packets.

--- core/usb_cx2.h
+++ core/usb_cx2.h
@@ -22,20 +22,24 @@ struct usb_cx2_state {
     uint32_t devaddr;   // 104
     uint32_t devtest;   // 108
     uint32_t phytest;   // 114
-    uint32_t cxfifo;    // 120
     uint32_t gimr_all;  // 130
     uint32_t gimr[3];   // 134-
     uint32_t gisr_all;  // 140
     uint32_t gisr[3];   // 144-
+    uint8_t rxzlp;      // 150
+    uint8_t txzlp;      // 154
     uint32_t epin[4];   // 160-
     uint32_t epout[4];  // 180-
     uint32_t epmap14;   // 1A0
+    uint32_t epmap58;   // 1A4
     uint32_t fifomap;   // 1A8
     uint32_t fifocfg;   // 1AC
     uint32_t dmafifo;   // 1C0
     uint32_t dmactrl;   // 1C8
 
     // There's also a dma0
+    uint32_t dma0ctrl;  // 300
+    uint32_t dma0addr;  // 304
     uint32_t dma1ctrl;  // 308
     uint32_t dma1addr;  // 30C
     uint32_t dma2ctrl;  // 310
@@ -46,8 +50,10 @@ struct usb_cx2_state {
     uint32_t dmasr;     // 328
     uint32_t dmamr;     // 32C
 
-    uint8_t dma_data[1024];
-    uint16_t dma_size;
+    uint8_t fifo_data[4][1024];
+    uint8_t cx_fifo_data[64];
+    uint16_t fifo_size[4];
+    uint8_t cx_fifo_size;
 
     uint32_t setup_packet[2];
 };
