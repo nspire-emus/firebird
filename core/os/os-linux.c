96756d89f8d8f2412975de1d7ba89b64a1c065ec
Fabian Vogt <fabian@ritter-vogt.de>
2020-08-29 13:43:30 +0200

    [x86_64 JIT] Support jumps/calls beyond a 32bit relative offset
    Currently the JIT emits relative jumps/calls when calling into
    asmcode_x86_64.S, but that only works if the JIT code area is close enough
    in memory. So far this was managed by not allowing the final executable to
    be position-independent and forcing the JIT code area to be placed at
    0x30000000.
    
    Implement support for 64bit absolute calls by emitting a table of addresses
    which can be used by rip-relative indirect jump/call instructions.
    
    This means PIC/PIE code is supported now, so drop the -no-pie enforcement
    and let mmap pick an appropriate location.

--- core/os/os-linux.c
+++ core/os/os-linux.c
@@ -75,14 +75,7 @@ void os_free(void *ptr, size_t size)
 
 void *os_alloc_executable(size_t size)
 {
-#if defined(__i386__) || defined(__x86_64__)
-    int map_flags = MAP_SHARED|MAP_ANON;
-#ifndef __APPLE__
-    // MAP_32BIT can't be used anymore on recent macOS. Looks fine elsewhere.
-    map_flags |= MAP_32BIT;
-#endif
-    void *ptr = mmap((void*)0x30000000, size, PROT_READ|PROT_WRITE|PROT_EXEC, map_flags, -1, 0);
-#elif defined(IS_IOS_BUILD)
+#if defined(IS_IOS_BUILD)
     void *ptr = mmap((void*)0x0, size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANON, -1, 0);
 #else
     void *ptr = mmap((void*)0x0, size, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_SHARED|MAP_ANON, -1, 0);
