Vogtinator/firebird-fork

Initial CX II support
- Boots (a bit)
- Needs some refactoring
- Some parts are missing suspend/resume support
- Flash creation dialog mostly useless
- No USB support (neither controller nor protocol)

core/mem.c
@@ -19,6 +19,7 @@
 #include "mem.h"
 #include "debug.h"
 #include "translate.h"
+#include "cx2.h"
 
 uint8_t   (*read_byte_map[64])(uint32_t addr);
 uint16_t  (*read_half_map[64])(uint32_t addr);
@@ -36,7 +37,7 @@ void bad_write_half(uint32_t addr, uint16_t value) { warn("Bad write_half: %08x 
 void bad_write_word(uint32_t addr, uint32_t value) { warn("Bad write_word: %08x %08x", addr, value); }
 
 uint8_t *mem_and_flags = NULL;
-struct mem_area_desc mem_areas[4];
+struct mem_area_desc mem_areas[5];
 
 void *phys_mem_ptr(uint32_t addr, uint32_t size) {
     unsigned int i;
@@ -135,33 +136,33 @@ void memory_write_word(uint32_t addr, uint32_t value) {
 struct apb_map_entry {
     uint32_t (*read)(uint32_t addr);
     void (*write)(uint32_t addr, uint32_t value);
-} apb_map[0x12];
+} apb_map[0x16];
 void apb_set_map(int entry, uint32_t (*read)(uint32_t addr), void (*write)(uint32_t addr, uint32_t value)) {
     apb_map[entry].read = read;
     apb_map[entry].write = write;
 }
 uint8_t apb_read_byte(uint32_t addr) {
-    if (addr >= 0x90120000) return bad_read_byte(addr);
+    if (addr >= 0x90150000) return bad_read_byte(addr);
     return apb_map[addr >> 16 & 31].read(addr & ~3) >> ((addr & 3) << 3);
 }
 uint16_t apb_read_half(uint32_t addr) {
-    if (addr >= 0x90120000) return bad_read_half(addr);
+    if (addr >= 0x90150000) return bad_read_half(addr);
     return apb_map[addr >> 16 & 31].read(addr & ~2) >> ((addr & 2) << 3);
 }
 uint32_t apb_read_word(uint32_t addr) {
-    if (addr >= 0x90120000) return bad_read_word(addr);
+    if (addr >= 0x90150000) return bad_read_word(addr);
     return apb_map[addr >> 16 & 31].read(addr);
 }
 void apb_write_byte(uint32_t addr, uint8_t value) {
-    if (addr >= 0x90120000) { bad_write_byte(addr, value); return; }
+    if (addr >= 0x90150000) { bad_write_byte(addr, value); return; }
     apb_map[addr >> 16 & 31].write(addr & ~3, value * 0x01010101u);
 }
 void apb_write_half(uint32_t addr, uint16_t value) {
-    if (addr >= 0x90120000) { bad_write_half(addr, value); return; }
+    if (addr >= 0x90150000) { bad_write_half(addr, value); return; }
     apb_map[addr >> 16 & 31].write(addr & ~2, value * 0x00010001u);
 }
 void apb_write_word(uint32_t addr, uint32_t value) {
-    if (addr >= 0x90120000) { bad_write_word(addr, value); return; }
+    if (addr >= 0x90150000) { bad_write_word(addr, value); return; }
     apb_map[addr >> 16 & 31].write(addr, value);
 }
 
@@ -215,13 +216,30 @@ bool memory_initialize(uint32_t sdram_size)
         return false;
     }
 
+    // Boot ROM
+    mem_areas[0].base = 0x0;
     mem_areas[0].size = 0x80000;
+
+    // SDRAM
     mem_areas[1].base = 0x10000000;
     mem_areas[1].size = sdram_size;
-    if (emulate_casplus) {
+
+    if (emulate_casplus)
+    {
         mem_areas[2].base = 0x20000000;
         mem_areas[2].size = 0x40000;
-    } else {
+    }
+    else if (emulate_cx2)
+    {
+        mem_areas[2].base = 0xA4000000;
+        mem_areas[2].size = 0x40000; // Double of CX
+
+        mem_areas[3].base = 0xA8000000;
+        mem_areas[3].size = 320 * 240 * 2; // One RGB565 frame
+    }
+    else
+    {
+        // Classic and CX
         mem_areas[2].base = 0xA4000000;
         mem_areas[2].size = 0x20000;
     }
@@ -243,6 +261,13 @@ bool memory_initialize(uint32_t sdram_size)
         mem_areas[3].base = 0xF0000000;
         mem_areas[3].size = mem_areas[0].size;
         mem_areas[3].ptr = mem_areas[0].ptr;
+    }
+
+    if (emulate_cx2)
+    {
+        mem_areas[4].base = 0xA0000000;
+        mem_areas[4].size = mem_areas[0].size;
+        mem_areas[4].ptr = mem_areas[0].ptr;
     }
 
     for (int i = 0; i < 64; i++) {
@@ -278,7 +303,7 @@ bool memory_initialize(uint32_t sdram_size)
     write_byte_map[0x90 >> 2] = apb_write_byte;
     write_half_map[0x90 >> 2] = apb_write_half;
     write_word_map[0x90 >> 2] = apb_write_word;
-    for (int i = 0; i < 0x12; i++) {
+    for (size_t i = 0; i < sizeof(apb_map)/sizeof(*apb_map); i++) {
         apb_map[i].read = bad_read_word;
         apb_map[i].write = bad_write_word;
     }
@@ -288,11 +313,10 @@ bool memory_initialize(uint32_t sdram_size)
     apb_set_map(0x06, watchdog_read, watchdog_write);
     add_reset_proc(watchdog_reset);
     apb_set_map(0x0A, misc_read, misc_write);
-    apb_set_map(0x0B, pmu_read, pmu_write);
-    add_reset_proc(pmu_reset);
     apb_set_map(0x0E, keypad_read, keypad_write);
     add_reset_proc(keypad_reset);
     apb_set_map(0x0F, hdq1w_read, hdq1w_write);
+
     add_reset_proc(hdq1w_reset);
     apb_set_map(0x11, unknown_9011_read, unknown_9011_write);
 
@@ -317,6 +341,7 @@ bool memory_initialize(uint32_t sdram_size)
     write_word_map[0xB4 >> 2] = usb_write_word;
 
     read_word_map[0xBC >> 2] = unknown_BC_read_word;
+    write_word_map[0xBC >> 2] = unknown_BC_write_word;
 
     read_word_map[0xC0 >> 2] = lcd_read_word;
     write_word_map[0xC0 >> 2] = lcd_write_word;
@@ -342,6 +367,8 @@ bool memory_initialize(uint32_t sdram_size)
         write_word_map[0x8F >> 2] = sdramctl_write_word;
 
         apb_set_map(0x01, timer_read, timer_write);
+        apb_set_map(0x0B, pmu_read, pmu_write);
+        add_reset_proc(pmu_reset);
         apb_set_map(0x0C, timer_read, timer_write);
         apb_set_map(0x0D, timer_read, timer_write);
         add_reset_proc(timer_reset);
@@ -363,15 +390,6 @@ bool memory_initialize(uint32_t sdram_size)
         write_word_map[0xDC >> 2] = int_write_word;
         add_reset_proc(int_reset);
     } else {
-        read_byte_map[0x80 >> 2] = nand_cx_read_byte;
-        read_word_map[0x80 >> 2] = nand_cx_read_word;
-        write_byte_map[0x80 >> 2] = nand_cx_write_byte;
-        write_word_map[0x80 >> 2] = nand_cx_write_word;
-
-        read_word_map[0x8F >> 2] = memctl_cx_read_word;
-        write_word_map[0x8F >> 2] = memctl_cx_write_word;
-        add_reset_proc(memctl_cx_reset);
-
         apb_set_map(0x01, timer_cx_read, timer_cx_write);
         apb_set_map(0x0C, timer_cx_read, timer_cx_write);
         apb_set_map(0x0D, timer_cx_read, timer_cx_write);
@@ -379,15 +397,46 @@ bool memory_initialize(uint32_t sdram_size)
         apb_set_map(0x02, serial_cx_read, serial_cx_write);
         add_reset_proc(serial_cx_reset);
         apb_set_map(0x03, unknown_cx_read, unknown_cx_write);
-        apb_set_map(0x04, unknown_cx_w_read, unknown_cx_w_write);
         apb_set_map(0x05, touchpad_cx_read, touchpad_cx_write);
         add_reset_proc(touchpad_cx_reset);
         apb_set_map(0x09, rtc_cx_read, rtc_cx_write);
 
-        read_word_map[0xB4 >> 2] = usb_read_word;
+        if(emulate_cx2)
+        {
+            // Second serial port - for now use the same state
+            apb_set_map(0x04, cx2_lcd_spi_read, cx2_lcd_spi_write);
+            apb_set_map(0x07, serial_cx_read, serial_cx_write);
+            apb_set_map(0x08, unknown_9008_read, unknown_9008_write);
+            apb_set_map(0x0B, adc_cx2_read_word, adc_cx2_write_word);
+            apb_set_map(0x12, memc_ddr_read, memc_ddr_write);
+            apb_set_map(0x13, bad_read_word, cx2_backlight_write);
+            apb_set_map(0x14, aladdin_pmu_read, aladdin_pmu_write);
+            add_reset_proc(aladdin_pmu_reset);
 
-        read_word_map[0xB8 >> 2] = sramctl_read_word;
-        write_word_map[0xB8 >> 2] = sramctl_write_word;
+            read_word_map[0xB8 >> 2] = spinand_cx2_read_word;
+            read_byte_map[0xB8 >> 2] = spinand_cx2_read_byte;
+            write_word_map[0xB8 >> 2] = spinand_cx2_write_word;
+            write_byte_map[0xB8 >> 2] = spinand_cx2_write_byte;
+            add_reset_proc(flash_spi_reset);
+        }
+        else
+        {
+            read_word_map[0x8F >> 2] = memctl_cx_read_word;
+            write_word_map[0x8F >> 2] = memctl_cx_write_word;
+            add_reset_proc(memctl_cx_reset);
+
+            apb_set_map(0x04, unknown_cx_w_read, unknown_cx_w_write);
+            apb_set_map(0x0B, pmu_read, pmu_write);
+            add_reset_proc(pmu_reset);
+
+            read_byte_map[0x80 >> 2] = nand_cx_read_byte;
+            read_word_map[0x80 >> 2] = nand_cx_read_word;
+            write_byte_map[0x80 >> 2] = nand_cx_write_byte;
+            write_word_map[0x80 >> 2] = nand_cx_write_word;
+
+            read_word_map[0xB8 >> 2] = sramctl_read_word;
+            write_word_map[0xB8 >> 2] = sramctl_write_word;
+        }
 
         read_word_map[0xDC >> 2] = int_cx_read_word;
         write_word_map[0xDC >> 2] = int_cx_write_word;

nspire-emus/firebird

core/mem.c
@@ -19,6 +19,7 @@
 #include "mem.h"
 #include "debug.h"
 #include "translate.h"
+#include "cx2.h"
 
 uint8_t   (*read_byte_map[64])(uint32_t addr);
 uint16_t  (*read_half_map[64])(uint32_t addr);
@@ -36,7 +37,7 @@ void bad_write_half(uint32_t addr, uint16_t value) { warn("Bad write_half: %08x 
 void bad_write_word(uint32_t addr, uint32_t value) { warn("Bad write_word: %08x %08x", addr, value); }
 
 uint8_t *mem_and_flags = NULL;
-struct mem_area_desc mem_areas[4];
+struct mem_area_desc mem_areas[5];
 
 void *phys_mem_ptr(uint32_t addr, uint32_t size) {
     unsigned int i;
@@ -135,33 +136,33 @@ void memory_write_word(uint32_t addr, uint32_t value) {
 struct apb_map_entry {
     uint32_t (*read)(uint32_t addr);
     void (*write)(uint32_t addr, uint32_t value);
-} apb_map[0x12];
+} apb_map[0x16];
 void apb_set_map(int entry, uint32_t (*read)(uint32_t addr), void (*write)(uint32_t addr, uint32_t value)) {
     apb_map[entry].read = read;
     apb_map[entry].write = write;
 }
 uint8_t apb_read_byte(uint32_t addr) {
-    if (addr >= 0x90120000) return bad_read_byte(addr);
+    if (addr >= 0x90150000) return bad_read_byte(addr);
     return apb_map[addr >> 16 & 31].read(addr & ~3) >> ((addr & 3) << 3);
 }
 uint16_t apb_read_half(uint32_t addr) {
-    if (addr >= 0x90120000) return bad_read_half(addr);
+    if (addr >= 0x90150000) return bad_read_half(addr);
     return apb_map[addr >> 16 & 31].read(addr & ~2) >> ((addr & 2) << 3);
 }
 uint32_t apb_read_word(uint32_t addr) {
-    if (addr >= 0x90120000) return bad_read_word(addr);
+    if (addr >= 0x90150000) return bad_read_word(addr);
     return apb_map[addr >> 16 & 31].read(addr);
 }
 void apb_write_byte(uint32_t addr, uint8_t value) {
-    if (addr >= 0x90120000) { bad_write_byte(addr, value); return; }
+    if (addr >= 0x90150000) { bad_write_byte(addr, value); return; }
     apb_map[addr >> 16 & 31].write(addr & ~3, value * 0x01010101u);
 }
 void apb_write_half(uint32_t addr, uint16_t value) {
-    if (addr >= 0x90120000) { bad_write_half(addr, value); return; }
+    if (addr >= 0x90150000) { bad_write_half(addr, value); return; }
     apb_map[addr >> 16 & 31].write(addr & ~2, value * 0x00010001u);
 }
 void apb_write_word(uint32_t addr, uint32_t value) {
-    if (addr >= 0x90120000) { bad_write_word(addr, value); return; }
+    if (addr >= 0x90150000) { bad_write_word(addr, value); return; }
     apb_map[addr >> 16 & 31].write(addr, value);
 }
 
@@ -215,13 +216,30 @@ bool memory_initialize(uint32_t sdram_size)
         return false;
     }
 
+    // Boot ROM
+    mem_areas[0].base = 0x0;
     mem_areas[0].size = 0x80000;
+
+    // SDRAM
     mem_areas[1].base = 0x10000000;
     mem_areas[1].size = sdram_size;
-    if (emulate_casplus) {
+
+    if (emulate_casplus)
+    {
         mem_areas[2].base = 0x20000000;
         mem_areas[2].size = 0x40000;
-    } else {
+    }
+    else if (emulate_cx2)
+    {
+        mem_areas[2].base = 0xA4000000;
+        mem_areas[2].size = 0x40000; // Double of CX
+
+        mem_areas[3].base = 0xA8000000;
+        mem_areas[3].size = 320 * 240 * 2; // One RGB565 frame
+    }
+    else
+    {
+        // Classic and CX
         mem_areas[2].base = 0xA4000000;
         mem_areas[2].size = 0x20000;
     }
@@ -243,6 +261,13 @@ bool memory_initialize(uint32_t sdram_size)
         mem_areas[3].base = 0xF0000000;
         mem_areas[3].size = mem_areas[0].size;
         mem_areas[3].ptr = mem_areas[0].ptr;
+    }
+
+    if (emulate_cx2)
+    {
+        mem_areas[4].base = 0xA0000000;
+        mem_areas[4].size = mem_areas[0].size;
+        mem_areas[4].ptr = mem_areas[0].ptr;
     }
 
     for (int i = 0; i < 64; i++) {
@@ -278,7 +303,7 @@ bool memory_initialize(uint32_t sdram_size)
     write_byte_map[0x90 >> 2] = apb_write_byte;
     write_half_map[0x90 >> 2] = apb_write_half;
     write_word_map[0x90 >> 2] = apb_write_word;
-    for (int i = 0; i < 0x12; i++) {
+    for (size_t i = 0; i < sizeof(apb_map)/sizeof(*apb_map); i++) {
         apb_map[i].read = bad_read_word;
         apb_map[i].write = bad_write_word;
     }
@@ -288,11 +313,10 @@ bool memory_initialize(uint32_t sdram_size)
     apb_set_map(0x06, watchdog_read, watchdog_write);
     add_reset_proc(watchdog_reset);
     apb_set_map(0x0A, misc_read, misc_write);
-    apb_set_map(0x0B, pmu_read, pmu_write);
-    add_reset_proc(pmu_reset);
     apb_set_map(0x0E, keypad_read, keypad_write);
     add_reset_proc(keypad_reset);
     apb_set_map(0x0F, hdq1w_read, hdq1w_write);
+
     add_reset_proc(hdq1w_reset);
     apb_set_map(0x11, unknown_9011_read, unknown_9011_write);
 
@@ -317,6 +341,7 @@ bool memory_initialize(uint32_t sdram_size)
     write_word_map[0xB4 >> 2] = usb_write_word;
 
     read_word_map[0xBC >> 2] = unknown_BC_read_word;
+    write_word_map[0xBC >> 2] = unknown_BC_write_word;
 
     read_word_map[0xC0 >> 2] = lcd_read_word;
     write_word_map[0xC0 >> 2] = lcd_write_word;
@@ -342,6 +367,8 @@ bool memory_initialize(uint32_t sdram_size)
         write_word_map[0x8F >> 2] = sdramctl_write_word;
 
         apb_set_map(0x01, timer_read, timer_write);
+        apb_set_map(0x0B, pmu_read, pmu_write);
+        add_reset_proc(pmu_reset);
         apb_set_map(0x0C, timer_read, timer_write);
         apb_set_map(0x0D, timer_read, timer_write);
         add_reset_proc(timer_reset);
@@ -363,15 +390,6 @@ bool memory_initialize(uint32_t sdram_size)
         write_word_map[0xDC >> 2] = int_write_word;
         add_reset_proc(int_reset);
     } else {
-        read_byte_map[0x80 >> 2] = nand_cx_read_byte;
-        read_word_map[0x80 >> 2] = nand_cx_read_word;
-        write_byte_map[0x80 >> 2] = nand_cx_write_byte;
-        write_word_map[0x80 >> 2] = nand_cx_write_word;
-
-        read_word_map[0x8F >> 2] = memctl_cx_read_word;
-        write_word_map[0x8F >> 2] = memctl_cx_write_word;
-        add_reset_proc(memctl_cx_reset);
-
         apb_set_map(0x01, timer_cx_read, timer_cx_write);
         apb_set_map(0x0C, timer_cx_read, timer_cx_write);
         apb_set_map(0x0D, timer_cx_read, timer_cx_write);
@@ -379,15 +397,46 @@ bool memory_initialize(uint32_t sdram_size)
         apb_set_map(0x02, serial_cx_read, serial_cx_write);
         add_reset_proc(serial_cx_reset);
         apb_set_map(0x03, fastboot_cx_read, fastboot_cx_write);
-        apb_set_map(0x04, spi_cx_read, spi_cx_write);
         apb_set_map(0x05, touchpad_cx_read, touchpad_cx_write);
         add_reset_proc(touchpad_cx_reset);
         apb_set_map(0x09, rtc_cx_read, rtc_cx_write);
 
-        read_word_map[0xB4 >> 2] = usb_read_word;
+        if(emulate_cx2)
+        {
+            // Second serial port - for now use the same state
+            apb_set_map(0x04, cx2_lcd_spi_read, cx2_lcd_spi_write);
+            apb_set_map(0x07, serial_cx_read, serial_cx_write);
+            apb_set_map(0x08, unknown_9008_read, unknown_9008_write);
+            apb_set_map(0x0B, adc_cx2_read_word, adc_cx2_write_word);
+            apb_set_map(0x12, memc_ddr_read, memc_ddr_write);
+            apb_set_map(0x13, bad_read_word, cx2_backlight_write);
+            apb_set_map(0x14, aladdin_pmu_read, aladdin_pmu_write);
+            add_reset_proc(aladdin_pmu_reset);
 
-        read_word_map[0xB8 >> 2] = sramctl_read_word;
-        write_word_map[0xB8 >> 2] = sramctl_write_word;
+            read_word_map[0xB8 >> 2] = spinand_cx2_read_word;
+            read_byte_map[0xB8 >> 2] = spinand_cx2_read_byte;
+            write_word_map[0xB8 >> 2] = spinand_cx2_write_word;
+            write_byte_map[0xB8 >> 2] = spinand_cx2_write_byte;
+            add_reset_proc(flash_spi_reset);
+        }
+        else
+        {
+            read_word_map[0x8F >> 2] = memctl_cx_read_word;
+            write_word_map[0x8F >> 2] = memctl_cx_write_word;
+            add_reset_proc(memctl_cx_reset);
+
+            apb_set_map(0x04, spi_cx_read, spi_cx_write);
+            apb_set_map(0x0B, pmu_read, pmu_write);
+            add_reset_proc(pmu_reset);
+
+            read_byte_map[0x80 >> 2] = nand_cx_read_byte;
+            read_word_map[0x80 >> 2] = nand_cx_read_word;
+            write_byte_map[0x80 >> 2] = nand_cx_write_byte;
+            write_word_map[0x80 >> 2] = nand_cx_write_word;
+
+            read_word_map[0xB8 >> 2] = sramctl_read_word;
+            write_word_map[0xB8 >> 2] = sramctl_write_word;
+        }
 
         read_word_map[0xDC >> 2] = int_cx_read_word;
         write_word_map[0xDC >> 2] = int_cx_write_word;