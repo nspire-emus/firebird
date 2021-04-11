Vogtinator/firebird-fork

Use streaming operations for snapshot IO
Reading from/writing to the snapshot explicitly instead of manipulating a full
copy in RAM has various advantages:
- Data goes directly from the source to the destination, without having a full
  uncompressed copy in between
- Avoid memory use of the full copy
- No need for a struct which includes all peripheral state
- Content is more flexible, as opposed to allowing flexible data only at the
  end

core/des.c
@@ -188,12 +188,10 @@ void des_write_word(uint32_t addr, uint32_t value) {
 
 bool des_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.des = des;
-    return true;
+    return write_to_snapshot(snapshot, &des, sizeof(des));
 }
 
 bool des_resume(const emu_snapshot *snapshot)
 {
-    des = snapshot->mem.des;
-    return true;
+    return read_from_snapshot(snapshot, &des, sizeof(des));
 }

core/emu.cpp
@@ -145,26 +145,6 @@ void throttle_interval_event(int index)
         throttle_timer_wait();
 }
 
-static size_t gzip_filesize(const char *path)
-{
-    #if __BYTE_ORDER == __LITTLE_ENDIAN
-        FILE *fp = fopen_utf8(path, "rb");
-        if(!fp)
-            return false;
-
-        // The last four bytes of a gzip file are the uncompressed size (% 2^32)
-        fseek(fp, -4, SEEK_END);
-        uint32_t ret = 0;
-        if(fread(&ret, 4, 1, fp) != 1)
-            ret = 0;
-
-        fclose(fp);
-        return ret;
-    #else
-        #error "Not implemented"
-    #endif
-}
-
 struct gui_busy_raii {
     gui_busy_raii() { gui_set_busy(true); }
     ~gui_busy_raii() { gui_set_busy(false); }
@@ -186,6 +166,16 @@ static void emu_reset()
     memory_reset();
 }
 
+bool read_from_snapshot(const emu_snapshot *snapshot, void *dest, int size)
+{
+    return gzread((gzFile)snapshot->stream_handle, dest, size) == size;
+}
+
+bool write_to_snapshot(emu_snapshot *snapshot, const void *src, int size)
+{
+    return gzwrite((gzFile)snapshot->stream_handle, src, size) == size;
+}
+
 bool emu_start(unsigned int port_gdb, unsigned int port_rdbg, const char *snapshot_file)
 {
     gui_busy_raii gui_busy;
@@ -193,10 +183,6 @@ bool emu_start(unsigned int port_gdb, unsigned int port_rdbg, const char *snapsh
     if(snapshot_file)
     {
         // Open snapshot
-        size_t snapshot_size = gzip_filesize(snapshot_file);
-        if(snapshot_size < sizeof(emu_snapshot))
-            return false;
-
         FILE *fp = fopen_utf8(snapshot_file, "rb");
         if(!fp)
             return false;
@@ -213,51 +199,50 @@ bool emu_start(unsigned int port_gdb, unsigned int port_rdbg, const char *snapsh
             return false;
         }
 
-        auto snapshot = (struct emu_snapshot *) malloc(snapshot_size);
-        if(!snapshot)
+        emu_snapshot snapshot = {};
+        snapshot.stream_handle = gzf;
+        // Read the header
+        if(!read_from_snapshot(&snapshot, &snapshot.header, sizeof(snapshot.header)))
         {
             gzclose(gzf);
             return false;
         }
 
-        if((size_t) gzread(gzf, snapshot, snapshot_size) != snapshot_size)
-        {
-            gzclose(gzf);
-            free(snapshot);
-            return false;
-        }
-        gzclose(gzf);
-
-        //sched_reset();
         sched.items[SCHED_THROTTLE].clock = CLOCK_27M;
         sched.items[SCHED_THROTTLE].proc = throttle_interval_event;
 
         // TODO: Max length
-        path_boot1 = std::string(snapshot->path_boot1);
-        path_flash = std::string(snapshot->path_flash);
-
-        // TODO: Pass snapshot_size to flash_resume to avoid reading after the buffer
+        path_boot1 = std::string(snapshot.header.path_boot1);
+        path_flash = std::string(snapshot.header.path_flash);
 
         // Resume components
-        uint32_t sdram_size;
-        if(snapshot->sig != SNAPSHOT_SIG
-                || snapshot->version != SNAPSHOT_VER
-                || !flash_resume(snapshot)
+        uint32_t sdram_size, dummy;
+        if(snapshot.header.sig != SNAPSHOT_SIG
+                || snapshot.header.version != SNAPSHOT_VER
+                || !flash_resume(&snapshot)
                 || !flash_read_settings(&sdram_size, &product, &features, &asic_user_flags)
-                || !cpu_resume(snapshot)
-                || !memory_resume(snapshot)
-                || !sched_resume(snapshot))
+                || !cpu_resume(&snapshot)
+                || !memory_resume(&snapshot)
+                || !sched_resume(&snapshot)
+                // Verify that EOF is next
+                || gzread(gzf, &dummy, sizeof(dummy)) != 0
+                || !gzeof(gzf))
         {
+            gzclose(gzf);
             emu_cleanup();
-            free(snapshot);
             return false;
         }
-        free(snapshot);
+
+        if(gzclose(gzf) != Z_OK)
+        {
+            emu_cleanup();
+            return false;
+        }
     }
     else
     {
         if (!flash_open(path_flash.c_str()))
-                return false;
+            return false;
 
         uint32_t sdram_size;
         flash_read_settings(&sdram_size, &product, &features, &asic_user_flags);
@@ -394,38 +379,26 @@ bool emu_suspend(const char *file)
         return false;
     }
 
-    size_t size = sizeof(emu_snapshot) + flash_suspend_flexsize();
-    auto snapshot = (struct emu_snapshot *) malloc(size);
-    if(!snapshot)
-    {
-        gzclose(gzf);
-        return false;
-    }
+    emu_snapshot snapshot = {};
+    snapshot.stream_handle = gzf;
 
-    snapshot->product = product;
-    snapshot->asic_user_flags = asic_user_flags;
+    snapshot.header.sig = SNAPSHOT_SIG;
+    snapshot.header.version = SNAPSHOT_VER;
     // TODO: Max length
-    strncpy(snapshot->path_boot1, path_boot1.c_str(), sizeof(snapshot->path_boot1) - 1);
-    strncpy(snapshot->path_flash, path_flash.c_str(), sizeof(snapshot->path_flash) - 1);
+    strncpy(snapshot.header.path_boot1, path_boot1.c_str(), sizeof(snapshot.header.path_boot1) - 1);
+    strncpy(snapshot.header.path_flash, path_flash.c_str(), sizeof(snapshot.header.path_flash) - 1);
 
-    if(!flash_suspend(snapshot)
-            || !cpu_suspend(snapshot)
-            || !sched_suspend(snapshot)
-            || !memory_suspend(snapshot))
+    if(!write_to_snapshot(&snapshot, &snapshot.header, sizeof(snapshot.header))
+            || !flash_suspend(&snapshot)
+            || !cpu_suspend(&snapshot)
+            || !memory_suspend(&snapshot)
+            || !sched_suspend(&snapshot))
     {
-        free(snapshot);
         gzclose(gzf);
         return false;
     }
 
-    snapshot->sig = SNAPSHOT_SIG;
-    snapshot->version = SNAPSHOT_VER;
-
-    bool success = (size_t) gzwrite(gzf, snapshot, size) == size;
-
-    free(snapshot);
-    gzclose(gzf);
-    return success;
+    return gzclose(gzf) == Z_OK;
 }
 
 void emu_cleanup()

core/lcd.c
@@ -428,12 +428,10 @@ write_control:
 
 bool lcd_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.lcd = lcd;
-    return true;
+    return write_to_snapshot(snapshot, &lcd, sizeof(lcd));
 }
 
 bool lcd_resume(const emu_snapshot *snapshot)
 {
-    lcd = snapshot->mem.lcd;
-    return true;
+    return read_from_snapshot(snapshot, &lcd, sizeof(lcd));
 }

core/serial.c
@@ -70,14 +70,12 @@ static serial_state serial;
 
 bool serial_resume(const emu_snapshot *snapshot)
 {
-    serial = snapshot->mem.serial;
-    return true;
+    return read_from_snapshot(snapshot, &serial, sizeof(serial));
 }
 
 bool serial_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.serial = serial;
-    return true;
+    return write_to_snapshot(snapshot, &serial, sizeof(serial));
 }
 
 static void serial_int_check() {
@@ -160,14 +158,12 @@ static serial_cx_state serial_cx;
 
 bool serial_cx_resume(const emu_snapshot *snapshot)
 {
-    serial_cx = snapshot->mem.serial_cx;
-    return true;
+    return read_from_snapshot(snapshot, &serial_cx, sizeof(serial_cx));
 }
 
 bool serial_cx_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.serial_cx = serial_cx;
-    return true;
+    return write_to_snapshot(snapshot, &serial_cx, sizeof(serial_cx));
 }
 
 static inline void serial_cx_int_check() {

core/usb.c
@@ -261,12 +261,10 @@ void usb_write_word(uint32_t addr, uint32_t value) {
 
 bool usb_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.usb = usb;
-    return true;
+    return write_to_snapshot(snapshot, &usb, sizeof(usb));
 }
 
 bool usb_resume(const emu_snapshot *snapshot)
 {
-    usb = snapshot->mem.usb;
-    return true;
+    return read_from_snapshot(snapshot, &usb, sizeof(usb));
 }

core/usb_cx2.cpp
@@ -542,12 +542,10 @@ uint16_t usb_cx2_read_half(uint32_t addr)
 
 bool usb_cx2_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.usb_cx2 = usb_cx2;
-    return true;
+    return write_to_snapshot(snapshot, &usb_cx2, sizeof(usb_cx2));
 }
 
 bool usb_cx2_resume(const emu_snapshot *snapshot)
 {
-    usb_cx2 = snapshot->mem.usb_cx2;
-    return true;
+    return read_from_snapshot(snapshot, &usb_cx2, sizeof(usb_cx2));
 }

nspire-emus/firebird

core/des.c
@@ -188,12 +188,10 @@ void des_write_word(uint32_t addr, uint32_t value) {
 
 bool des_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.des = des;
-    return true;
+    return snapshot_write(snapshot, &des, sizeof(des));
 }
 
 bool des_resume(const emu_snapshot *snapshot)
 {
-    des = snapshot->mem.des;
-    return true;
+    return snapshot_read(snapshot, &des, sizeof(des));
 }

core/emu.cpp
@@ -145,26 +145,6 @@ void throttle_interval_event(int index)
         throttle_timer_wait();
 }
 
-static size_t gzip_filesize(const char *path)
-{
-    #if __BYTE_ORDER == __LITTLE_ENDIAN
-        FILE *fp = fopen_utf8(path, "rb");
-        if(!fp)
-            return false;
-
-        // The last four bytes of a gzip file are the uncompressed size (% 2^32)
-        fseek(fp, -4, SEEK_END);
-        uint32_t ret = 0;
-        if(fread(&ret, 4, 1, fp) != 1)
-            ret = 0;
-
-        fclose(fp);
-        return ret;
-    #else
-        #error "Not implemented"
-    #endif
-}
-
 struct gui_busy_raii {
     gui_busy_raii() { gui_set_busy(true); }
     ~gui_busy_raii() { gui_set_busy(false); }
@@ -186,6 +166,16 @@ static void emu_reset()
     memory_reset();
 }
 
+bool snapshot_read(const emu_snapshot *snapshot, void *dest, int size)
+{
+    return gzread((gzFile)snapshot->stream_handle, dest, size) == size;
+}
+
+bool snapshot_write(emu_snapshot *snapshot, const void *src, int size)
+{
+    return gzwrite((gzFile)snapshot->stream_handle, src, size) == size;
+}
+
 bool emu_start(unsigned int port_gdb, unsigned int port_rdbg, const char *snapshot_file)
 {
     gui_busy_raii gui_busy;
@@ -193,10 +183,6 @@ bool emu_start(unsigned int port_gdb, unsigned int port_rdbg, const char *snapsh
     if(snapshot_file)
     {
         // Open snapshot
-        size_t snapshot_size = gzip_filesize(snapshot_file);
-        if(snapshot_size < sizeof(emu_snapshot))
-            return false;
-
         FILE *fp = fopen_utf8(snapshot_file, "rb");
         if(!fp)
             return false;
@@ -213,51 +199,50 @@ bool emu_start(unsigned int port_gdb, unsigned int port_rdbg, const char *snapsh
             return false;
         }
 
-        auto snapshot = (struct emu_snapshot *) malloc(snapshot_size);
-        if(!snapshot)
+        emu_snapshot snapshot = {};
+        snapshot.stream_handle = gzf;
+        // Read the header
+        if(!snapshot_read(&snapshot, &snapshot.header, sizeof(snapshot.header)))
         {
             gzclose(gzf);
             return false;
         }
 
-        if((size_t) gzread(gzf, snapshot, snapshot_size) != snapshot_size)
-        {
-            gzclose(gzf);
-            free(snapshot);
-            return false;
-        }
-        gzclose(gzf);
-
-        //sched_reset();
         sched.items[SCHED_THROTTLE].clock = CLOCK_27M;
         sched.items[SCHED_THROTTLE].proc = throttle_interval_event;
 
         // TODO: Max length
-        path_boot1 = std::string(snapshot->path_boot1);
-        path_flash = std::string(snapshot->path_flash);
-
-        // TODO: Pass snapshot_size to flash_resume to avoid reading after the buffer
+        path_boot1 = std::string(snapshot.header.path_boot1);
+        path_flash = std::string(snapshot.header.path_flash);
 
         // Resume components
-        uint32_t sdram_size;
-        if(snapshot->sig != SNAPSHOT_SIG
-                || snapshot->version != SNAPSHOT_VER
-                || !flash_resume(snapshot)
+        uint32_t sdram_size, dummy;
+        if(snapshot.header.sig != SNAPSHOT_SIG
+                || snapshot.header.version != SNAPSHOT_VER
+                || !flash_resume(&snapshot)
                 || !flash_read_settings(&sdram_size, &product, &features, &asic_user_flags)
-                || !cpu_resume(snapshot)
-                || !memory_resume(snapshot)
-                || !sched_resume(snapshot))
+                || !cpu_resume(&snapshot)
+                || !memory_resume(&snapshot)
+                || !sched_resume(&snapshot)
+                // Verify that EOF is next
+                || gzread(gzf, &dummy, sizeof(dummy)) != 0
+                || !gzeof(gzf))
         {
+            gzclose(gzf);
             emu_cleanup();
-            free(snapshot);
             return false;
         }
-        free(snapshot);
+
+        if(gzclose(gzf) != Z_OK)
+        {
+            emu_cleanup();
+            return false;
+        }
     }
     else
     {
         if (!flash_open(path_flash.c_str()))
-                return false;
+            return false;
 
         uint32_t sdram_size;
         flash_read_settings(&sdram_size, &product, &features, &asic_user_flags);
@@ -394,38 +379,26 @@ bool emu_suspend(const char *file)
         return false;
     }
 
-    size_t size = sizeof(emu_snapshot) + flash_suspend_flexsize();
-    auto snapshot = (struct emu_snapshot *) malloc(size);
-    if(!snapshot)
-    {
-        gzclose(gzf);
-        return false;
-    }
+    emu_snapshot snapshot = {};
+    snapshot.stream_handle = gzf;
 
-    snapshot->product = product;
-    snapshot->asic_user_flags = asic_user_flags;
+    snapshot.header.sig = SNAPSHOT_SIG;
+    snapshot.header.version = SNAPSHOT_VER;
     // TODO: Max length
-    strncpy(snapshot->path_boot1, path_boot1.c_str(), sizeof(snapshot->path_boot1) - 1);
-    strncpy(snapshot->path_flash, path_flash.c_str(), sizeof(snapshot->path_flash) - 1);
+    strncpy(snapshot.header.path_boot1, path_boot1.c_str(), sizeof(snapshot.header.path_boot1) - 1);
+    strncpy(snapshot.header.path_flash, path_flash.c_str(), sizeof(snapshot.header.path_flash) - 1);
 
-    if(!flash_suspend(snapshot)
-            || !cpu_suspend(snapshot)
-            || !sched_suspend(snapshot)
-            || !memory_suspend(snapshot))
+    if(!snapshot_write(&snapshot, &snapshot.header, sizeof(snapshot.header))
+            || !flash_suspend(&snapshot)
+            || !cpu_suspend(&snapshot)
+            || !memory_suspend(&snapshot)
+            || !sched_suspend(&snapshot))
     {
-        free(snapshot);
         gzclose(gzf);
         return false;
     }
 
-    snapshot->sig = SNAPSHOT_SIG;
-    snapshot->version = SNAPSHOT_VER;
-
-    bool success = (size_t) gzwrite(gzf, snapshot, size) == size;
-
-    free(snapshot);
-    gzclose(gzf);
-    return success;
+    return gzclose(gzf) == Z_OK;
 }
 
 void emu_cleanup()

core/lcd.c
@@ -428,12 +428,10 @@ write_control:
 
 bool lcd_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.lcd = lcd;
-    return true;
+    return snapshot_write(snapshot, &lcd, sizeof(lcd));
 }
 
 bool lcd_resume(const emu_snapshot *snapshot)
 {
-    lcd = snapshot->mem.lcd;
-    return true;
+    return snapshot_read(snapshot, &lcd, sizeof(lcd));
 }

core/serial.c
@@ -70,14 +70,12 @@ static serial_state serial;
 
 bool serial_resume(const emu_snapshot *snapshot)
 {
-    serial = snapshot->mem.serial;
-    return true;
+    return snapshot_read(snapshot, &serial, sizeof(serial));
 }
 
 bool serial_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.serial = serial;
-    return true;
+    return snapshot_write(snapshot, &serial, sizeof(serial));
 }
 
 static void serial_int_check() {
@@ -160,14 +158,12 @@ static serial_cx_state serial_cx;
 
 bool serial_cx_resume(const emu_snapshot *snapshot)
 {
-    serial_cx = snapshot->mem.serial_cx;
-    return true;
+    return snapshot_read(snapshot, &serial_cx, sizeof(serial_cx));
 }
 
 bool serial_cx_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.serial_cx = serial_cx;
-    return true;
+    return snapshot_write(snapshot, &serial_cx, sizeof(serial_cx));
 }
 
 static inline void serial_cx_int_check() {

core/usb.c
@@ -261,12 +261,10 @@ void usb_write_word(uint32_t addr, uint32_t value) {
 
 bool usb_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.usb = usb;
-    return true;
+    return snapshot_write(snapshot, &usb, sizeof(usb));
 }
 
 bool usb_resume(const emu_snapshot *snapshot)
 {
-    usb = snapshot->mem.usb;
-    return true;
+    return snapshot_read(snapshot, &usb, sizeof(usb));
 }

core/usb_cx2.cpp
@@ -542,12 +542,10 @@ uint16_t usb_cx2_read_half(uint32_t addr)
 
 bool usb_cx2_suspend(emu_snapshot *snapshot)
 {
-    snapshot->mem.usb_cx2 = usb_cx2;
-    return true;
+    return snapshot_write(snapshot, &usb_cx2, sizeof(usb_cx2));
 }
 
 bool usb_cx2_resume(const emu_snapshot *snapshot)
 {
-    usb_cx2 = snapshot->mem.usb_cx2;
-    return true;
+    return snapshot_read(snapshot, &usb_cx2, sizeof(usb_cx2));
 }